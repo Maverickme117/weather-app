5c5c73d375392fd5f490d75deec76d2b8519bd44
Maverickme117 <163622603+Maverickme117@users.noreply.github.com>
2024-08-08 12:36:38 +0530

    Update Credit.js

--- React-Weather-App-main/src/components/Credit.js
+++ React-Weather-App-main/src/components/Credit.js
@@ -5,7 +5,7 @@ export default function Credit() {
     <div class="credit">
     <p>
       Coded by
-      <a href="https://github.com/Maverickme117/Myportfolio.git"> Rahul K</a>
+      <a href="#"> Rahul K</a>
        <a href="https://github.com/Maverickme117/weather-app.git">Open sourced on Github.</a> Hosted on Netlify
     </p>
   </div>
